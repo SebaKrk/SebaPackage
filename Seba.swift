dc0df109945167e105979d0b1b630d71fb61ef1e
Sebastian Ściuba <sebastian.sciuba@farmaprom.pl>
2023-11-28 21:15:35 +0100

    new

---
+++ Seba.swift
@@ -0,0 +1,18 @@
+//
+//  Seba.swift
+//  SebaPackage
+//
+//  Created by Sebastian Ściuba on 28/11/2023.
+//
+
+import SwiftUI
+
+extension View {
+    public static func sebaCenter() -> some View {
+        HStack {
+            Spacer()
+            self
+            Spacer
+        }
+    }
+}
